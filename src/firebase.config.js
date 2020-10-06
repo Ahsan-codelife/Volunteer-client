1d15318b89fcb44b124d969dd51a8aa1f0ae72bf
Ahsan-codelife <65127583+Ahsan-codelife@users.noreply.github.com>
2020-10-06 15:33:47 +0600

    half

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDoj16b-ddZy9NudpoT2Ec07WxuQdIIcdc",
+    authDomain: "volunteer-network-a5800.firebaseapp.com",
+    databaseURL: "https://volunteer-network-a5800.firebaseio.com",
+    projectId: "volunteer-network-a5800",
+    storageBucket: "volunteer-network-a5800.appspot.com",
+    messagingSenderId: "181086387769",
+    appId: "1:181086387769:web:eadb72b7537a7a4e6357e8"
+  };
+
+export default firebaseConfig;
